edd14708fe55713886db68f42b11763a18bc9b06
Jeremey Schrack <jdschrack@outlook.com>
2019-11-14 09:45:30 -0500

    Saving the code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RockPluginInstaller
-A tool to test installation of Rock Plugins
+A tool to test installation of Rock Plugins.  This tool was created to test plugin installation using the same methods implemented in Rock RMS.  
+
+This tool is not intended to be used to bypass the Rock Shop, only to allow developers the ability to test their plugin package before sending to Rock for verification and distribution.
+
+## Setup
+1.	Open \TestRockInstall\App.config.
+2.  Add your connection string to the Rock Server you are testing with by edditing the following:
+```XML
+  <connectionStrings>
+    <add name="RockContext" connectionString="{EnterConnectionStringToRockServer}" providerName="System.Data.SqlClient"/>
+  </connectionStrings>
+```
+3.	Edit Program.cs and replace the packagePath (line 14) with the path to the package you are testing.
+```C#
+var packagePath = @"C:\files\Packages\my.plugin"; //Path to plugin to test.
+```
+4.	Edit Program.cs and replace the rockServerDirectory value (line 23) with the path to where Rock is installed.
+```C#
+var rockServerDirectory = @"C:\code\RockKIT\RockWeb"; //Path to RockServer files
+```
---
+++ TestRockInstall/Program.cs
@@ -0,0 +1,144 @@
+﻿using Rock;
+using Rock.Data;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace TestRockInstall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\files\Packages\com.securegive.rockplugin.2.3.plugin";
+
+            ProcessInstall(path);
+
+            Console.ReadKey();
+        }
+
+        static void ProcessInstall(string packagePath)
+        {
+            var appRoot = @"C:\code\RockKIT\RockWeb";
+
+            try
+            {
+                using (var packageZip = ZipFile.OpenRead(packagePath))
+                {                    
+                    foreach (var entry in packageZip.Entries.Where(x => x.FullName.StartsWith("content/", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        string fullPath = Path.Combine(appRoot, entry.FullName.Replace("content/", "")).Replace("/", "\\");
+                        var directory = Path.GetDirectoryName(fullPath).Replace("content/", "");
+                        Console.WriteLine(directory);
+                        if (entry.Length != 0)
+                        {
+                            createDirectory(directory); 
+                            entry.ExtractToFile(fullPath, true);
+                        }                        
+                    }
+
+                    processSql(packageZip);
+
+                    try
+                    {
+                        var deleteListEntry = packageZip.Entries.Where(e => e.FullName == "install/deletefile.lst").FirstOrDefault();
+                        if (deleteListEntry != null)
+                        {
+
+                            string deleteList = System.Text.Encoding.Default.GetString(deleteListEntry.Open().ReadBytesToEnd());
+
+                            string[] itemsToDelete = deleteList.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+                            foreach (string deleteItem in itemsToDelete)
+                            {
+                                if (!string.IsNullOrWhiteSpace(deleteItem))
+                                {
+                                    string deleteItemFullPath = appRoot + deleteItem;
+                                    Console.WriteLine($"Deleting {deleteItemFullPath}");
+
+                                    if (Directory.Exists(deleteItemFullPath))
+                                    {
+                                        Directory.Delete(deleteItemFullPath, true);
+                                    }
+
+                                    if (File.Exists(deleteItemFullPath))
+                                    {
+                                        File.Delete(deleteItemFullPath);
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error Modifying Files: An error occurred while modifying files. \nError: {ex.Message}\nSource: {ex.Source}\nTrace: {ex.StackTrace}");
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void processDeleteFile(ZipArchiveEntry deleteListEntry)
+        {
+
+        }
+
+        private static void processSql(ZipArchive packageZip)
+        {
+            try
+            {
+                var sqlInstallEntry = packageZip.Entries.Where(e => e.FullName == "install/run.sql").FirstOrDefault();
+                if (sqlInstallEntry != null)
+                {
+                    string sqlScript = System.Text.Encoding.Default.GetString(sqlInstallEntry.Open().ReadBytesToEnd());
+
+                    if (!string.IsNullOrWhiteSpace(sqlScript))
+                    {
+                        using (var context = new RockContext())
+                        {
+                            context.Database.Log = s => Console.WriteLine(s);
+                            context.Database.ExecuteSqlCommand(sqlScript);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Updating Database. An error occurred while updating the database. \nError: {ex.Message}");
+                return;
+            }
+        }
+
+        private static void createDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+
+        private static string buildRockShopDir(string appRoot)
+        {
+            var baseDirectory = Path.Combine(appRoot, "RockShop");
+            createDirectory(baseDirectory);
+
+            return baseDirectory;
+        }
+
+        static string baseDirectoryPath()
+        {
+            var baseDirectory = @"c:\files";
+            createDirectory(baseDirectory);
+
+            return baseDirectory;
+        }
+
+
+    }
+}
